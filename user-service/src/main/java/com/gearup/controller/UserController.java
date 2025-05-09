d0983f5c0830526f82a316ee0f2eeccee2043705
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-05-09 22:15:52 +0530

    user endpoint is created

--- user-service/src/main/java/com/gearup/controller/UserController.java
+++ user-service/src/main/java/com/gearup/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.gearup.controller;
 
+import com.gearup.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/api/users")
+    public User getUser(){
+        User user = new User();
+        user.setEmail("sam@gmail.com");
+        user.setFullName("Sam Collin");
+        user.setPhone("+94761234456");
+        user.setRole("Customer");
+        return  user;
+    }
 }
