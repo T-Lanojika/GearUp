a570736637515d4e68f2bbfce6c8dda6f12dcd6a
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-05-20 23:39:33 +0530

    serviceStation microservice error fixed

---
+++ serviceStation service/serviceStation-service/src/main/java/com/gearup/ServiceStationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStationServiceApplication.class, args);
+	}
+
+}
---
+++ serviceStation service/serviceStation-service/src/test/java/com/gearup/ServiceStationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
