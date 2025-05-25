06b97508fcf98984fc2262020c4b83585cc19a83
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-05-25 22:31:54 +0530

    serviceStation service is created

--- serviceStation service/serviceStation-service/src/main/java/com/gearup/service/ServiceStation.java
+++ serviceStation service/serviceStation-service/src/main/java/com/gearup/service/ServiceStation.java
@@ -2,8 +2,21 @@ package com.gearup.service;
 
 import com.gearup.model.serviceStation;
 import com.gearup.payload.dto.StationDTO;
+import com.gearup.payload.dto.UserDTO;
+
+import java.util.List;
 
 public interface ServiceStation {
-    serviceStation createStation(StationDTO station, userDTO user);
+    serviceStation createStation(StationDTO station, UserDTO user);
+
+    serviceStation updateStation(StationDTO station, UserDTO user,Long stationId);
+
+    List<serviceStation> getAllStations();
+
+    serviceStation getStationById(Long stationId);
+
+    serviceStation getStationByOwnerId(Long ownerId);
+
+    List<serviceStation> searchStationByCity(String city);
 
 }
