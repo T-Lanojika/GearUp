5f7637f65bd64534743573e064fe32298b2af0a7
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-05-26 23:09:06 +0530

    StationMapper is created

---
+++ serviceStation service/serviceStation-service/src/main/java/com/gearup/mapper/StationMapper.java
@@ -0,0 +1,22 @@
+package com.gearup.mapper;
+
+import com.gearup.model.serviceStation;
+import com.gearup.payload.dto.StationDTO;
+
+public class StationMapper {
+
+    public static StationDTO mapToDTO(serviceStation station) {
+        StationDTO stationDTO = new StationDTO();
+        stationDTO.setId(station.getId());
+        stationDTO.setName(station.getName());
+        stationDTO.setAddress(station.getAddress());
+        stationDTO.setCity(station.getCity());
+        stationDTO.setImages(station.getImages());
+        stationDTO.setCloseTime(station.getCloseTime());
+        stationDTO.setOpenTime(station.getOpenTime());
+        stationDTO.setPhoneNumber(station.getPhoneNumber());
+        stationDTO.setOwnerId(station.getOwnerId());
+
+        return stationDTO;
+    }
+}
