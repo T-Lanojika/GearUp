0f94885d4892d80dfabf08cb364cbcad0cd1285e
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-15 17:18:14 +0530

    service offering service is added

--- service offering/service-offering/src/main/java/com/gearup/service/ServiceOfferingService.java
+++ service offering/service-offering/src/main/java/com/gearup/service/ServiceOfferingService.java
@@ -5,8 +5,16 @@ import com.gearup.dto.ServiceDTO;
 import com.gearup.dto.StationDTO;
 import com.gearup.model.ServiceOffering;
 
+import java.util.Set;
+
 public interface ServiceOfferingService {
     ServiceOffering createService(StationDTO stationDTO,
                                   ServiceDTO serviceDTO,
                                   CategoryDTO categoryDTO);
+
+    ServiceOffering updateService(Long serviceId,ServiceOffering service);
+
+    Set<ServiceOffering> getAllServiceByStationId(Long stationId, Long categoryId);
+
+    Set<ServiceOffering> getServiceById(Set<Long> ids);
 }
