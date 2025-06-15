6db3018f17c69e64b5a733e78ecf55f12d96a986
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-15 17:17:42 +0530

    service offering repository is added

---
+++ service offering/service-offering/src/main/java/com/gearup/repository/ServiceOfferingRepository.java
@@ -0,0 +1,9 @@
+package com.gearup.repository;
+
+import com.gearup.model.ServiceOffering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering,Long> {
+
+
+}
