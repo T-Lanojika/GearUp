05527c764023fc926359b2522bc1753ae97bdfa2
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-15 11:03:37 +0530

    getCategoryById is added to the controller

--- category-service/src/main/java/com/gearup/controller/CategoryController.java
+++ category-service/src/main/java/com/gearup/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.gearup.model.Category;
 import com.gearup.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,20 @@ import java.util.Set;
 public class CategoryController {
     private  final CategoryService categoryService;
 
+    @GetMapping("/station/{id}")
     public ResponseEntity<Set<Category>> getCategoriesByStation(
             @PathVariable Long id
     ){
         Set<Category> categories=categoryService.getAllCategoriesByStation(id);
         return ResponseEntity.ok(categories);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(
+            @PathVariable Long id
+    ) throws Exception {
+        Category category=categoryService.getCategoryById(id);
+        return ResponseEntity.ok(category);
     }
 }
