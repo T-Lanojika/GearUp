7fef364386739c86c32a9105487a06a65ee5276c
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-22 23:29:00 +0530

    Booking Repository is added

--- booking-service/src/main/java/com/gearup/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/gearup/service/impl/BookingServiceImpl.java
@@ -7,15 +7,32 @@ import com.gearup.dto.StationDTO;
 import com.gearup.dto.UserDTO;
 import com.gearup.model.Booking;
 import com.gearup.model.StationReport;
+import com.gearup.repository.BookingRepository;
 import com.gearup.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
+@Service
+@RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
+
+    private final BookingRepository bookingRepository;
+
     @Override
-    public Booking createBooking(BookingRequest booking, UserDTO ser, StationDTO station, Set<ServiceDTO> serviceDTOSet) {
+    public Booking createBooking(BookingRequest booking,
+                                 UserDTO user,
+                                 StationDTO station,
+                                 Set<ServiceDTO> serviceDTOSet) {
+        int totalDuration = serviceDTOSet.stream().mapToInt(ServiceDTO::getDuration).sum();
+
+        LocalDateTime bookingStartTime = booking.getStartTime();
+        LocalDateTime bookingEndTime = bookingStartTime.plusMinutes(totalDuration);
+
         return null;
     }
 
