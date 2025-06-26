a918311a1d1231e9fbbc0d79ea99782a842bf5c9
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-26 21:16:24 +0530

    razorpay,strike values are added

---
+++ payment-service/src/main/java/com/gearup/repository/PaymentOrderRepository.java
@@ -0,0 +1,8 @@
+package com.gearup.repository;
+
+import com.gearup.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+    PaymentOrder findByPaymentLinkId(String paymentLinkId);
+}
--- payment-service/src/main/java/com/gearup/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/gearup/service/impl/PaymentServiceImpl.java
@@ -5,10 +5,29 @@ import com.gearup.model.PaymentOrder;
 import com.gearup.payload.response.PaymentLinkResponse;
 import com.gearup.payload.response.dto.BookingDTO;
 import com.gearup.payload.response.dto.UserDTO;
+import com.gearup.repository.PaymentOrderRepository;
 import com.gearup.service.PaymentService;
 import com.razorpay.PaymentLink;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentOrderRepository paymentOrderRepository;
+
+    @Value("${stripe.api.key}")
+    private String stripeSecretKey;
+
+    @Value("${razorpay.api.key}")
+    private String razorpayApiKey;
+
+    @Value("${razorpay.api.secret}")
+    private String razorpayApiSecret;
+
+
     @Override
     public PaymentLinkResponse createOrder(UserDTO user, BookingDTO booking, PaymentMethod paymentMethod) {
         return null;
