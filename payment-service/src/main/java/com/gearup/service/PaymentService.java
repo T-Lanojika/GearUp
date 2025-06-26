49add31d7213a8e4182bc09b5e807f9199f521ce
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-26 20:00:01 +0530

    DTO is added and services are created

--- payment-service/src/main/java/com/gearup/service/PaymentService.java
+++ payment-service/src/main/java/com/gearup/service/PaymentService.java
@@ -1,4 +1,18 @@
 package com.gearup.service;
 
+import com.gearup.domain.PaymentMethod;
+import com.gearup.model.PaymentOrder;
+import com.gearup.payload.response.PaymentLinkResponse;
+import com.gearup.payload.response.dto.BookingDTO;
+import com.gearup.payload.response.dto.UserDTO;
+
 public interface PaymentService {
+
+    PaymentLinkResponse createOrder(UserDTO user,
+                                    BookingDTO booking,
+                                    PaymentMethod paymentMethod);
+
+    PaymentOrder getPaymentOrderById(Long id);
+
+    PaymentOrder getPaymentOrderByPaymentId(String paymentId);
 }
