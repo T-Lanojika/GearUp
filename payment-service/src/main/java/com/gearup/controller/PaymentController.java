adc99f2582cfb71ce4dd161809b1bf67f4a6f9d0
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-06-26 22:45:59 +0530

    getPaymentOrderById controller is added

--- payment-service/src/main/java/com/gearup/controller/PaymentController.java
+++ payment-service/src/main/java/com/gearup/controller/PaymentController.java
@@ -1,4 +1,46 @@
 package com.gearup.controller;
 
+import com.gearup.domain.PaymentMethod;
+import com.gearup.model.PaymentOrder;
+import com.gearup.payload.response.PaymentLinkResponse;
+import com.gearup.payload.response.dto.BookingDTO;
+import com.gearup.payload.response.dto.UserDTO;
+import com.gearup.service.PaymentService;
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private  final PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<PaymentLinkResponse> createPaymentLink(
+            @RequestBody BookingDTO booking,
+            @RequestParam PaymentMethod paymentMethod
+            ) throws StripeException, RazorpayException {
+        UserDTO user = new UserDTO();
+        user.setFullName("Kisho");
+        user.setEmail("kisho39@gmail.com");
+        user.setId(1L);
+
+        PaymentLinkResponse res=paymentService.createOrder(user,booking,paymentMethod);
+
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/create")
+    public ResponseEntity<PaymentOrder> getPaymentOrderById(
+            @PathVariable Long paymentOrderId
+    ) throws Exception {
+
+        PaymentOrder res=paymentService.getPaymentOrderById(paymentOrderId);
+        return ResponseEntity.ok(res);
+
+    }
 }
